74a04e947ea828c5667d714a78d03aca611ab491
Mrak9087 <maks19-88@mail.ru>
2023-06-17 21:53:58 +0400

    feat: add component MainSection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Container from "./components/Container";
+import MainSection from "./components/MainSection";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <MainSection />
+    </Container>
+  )
 }
 
 export default App;
