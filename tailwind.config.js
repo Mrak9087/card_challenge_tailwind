c0d994220229c783072625e82d208d1db3ac35e8
Mrak9087 <maks19-88@mail.ru>
2023-06-17 13:32:02 +0400

    feat: add settings to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'confirm':'#220930',
+      },
+      backgroundImage:{
+        'desktop':"url(./images/bg-main-desktop.png)",
+        'mobile':"url(./images/bg-main-mobile.png)",
+        'cardFront': "url(./images/bg-card-front.png)",
+        'cardBack': "url(./images/bg-card-back.png)",
+      },
+      screens: {
+        xl: '1440px',
+      }
+    },
   },
   plugins: [],
 };
